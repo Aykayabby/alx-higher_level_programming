74df45096fed51e75c69c02d7d19b6fa2f72d85b
Aykayabby <19-30gp017@students.unilorin.edu.ng>
2022-06-30 06:37:44 -0700

    commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(1, 101):
         tab = ""
         if ((i % 3 == 0) and (i % 5 == 0)):
-        tab = "FB"
+            tab = "FB"
         elif i % 3 == 0:
             tab = "F"
         elif i % 5 == 0:
