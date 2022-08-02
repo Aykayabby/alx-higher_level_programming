86f32e9f97c6913b2ce3b5cfc100e013253812bd
Aykayabby <19-30gp017@students.unilrin.edu.ng>
2022-08-02 06:40:02 -0700

    returns rctangle properties

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"A module that defines a rectangle"
+
+
+class Rectangle:
+    "A rectangle class"
+
+    def __init__(self, width=0, height=0):
+        "init method
+
+        args:
+            width: The width in int
+            height: The height in int"
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        "Returns width of the property
+
+        raises error when width is not and integer or is less than 0"
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be aan integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        "Returns height of the property
+
+        raises error when the height is not an integer or is less than 0"
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
