e52016ff95b6ad3a535504960873c8bba208d0a1
Aykayabby <19-30gp017@students.unilrin.edu.ng>
2022-08-03 09:13:32 -0700

    commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module which contains BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A BaseGeometry class"""
+    def area(self):
+        """Afunction that raises an exception"""
+        raise Exception("area() is not implemented")
