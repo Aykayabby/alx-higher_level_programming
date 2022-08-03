9afd96ac6df853eb1ec74fb8a0561c405e546c58
Aykayabby <19-30gp017@students.unilrin.edu.ng>
2022-08-03 08:48:08 -0700

    commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """checks if obj is an instance of a_class"""
-    return isinstance(obj, a_class)
+    return type(obj) != a_class and issubclass(type(obj), a_class)
