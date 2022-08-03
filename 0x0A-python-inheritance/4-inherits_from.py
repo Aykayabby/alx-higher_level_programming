bf2e511978faf299cbe94ca374a19378b23e1a6d
Aykayabby <19-30gp017@students.unilrin.edu.ng>
2022-08-03 08:36:42 -0700

    commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module that checks if object is an instance of a class"""
+
+
+def inherits_from(obj, a_class):
+    """checks if obj is an instance of a_class"""
+    return isinstance(obj, a_class)
